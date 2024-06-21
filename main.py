1a5db23b55b5c4825edd3e87c657db4dca45e93a
Adrien DEBADIER <adriendebadier@hotmail.com>
2024-06-21 12:29:33 +0200

    add token into .env

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import discord
 
+import os
+from dotenv import load_dotenv
 from src.settings.variables import *
 from src.settings.configs import getIntents
 from src.events import register_events
-from src.settings.token import BOT_TOKEN
-# Create a file token.py in src/settings and put the variable BOT_TOKEN="<token>"
+
+load_dotenv()
 
 def main():
 	intents = getIntents()
 	client = discord.Client(intents=intents)
 	client.tree = discord.app_commands.CommandTree(client)
 	register_events(client)
-	client.run(BOT_TOKEN)
+
+	bot_token = os.getenv("Z_01_GROUP_MAKER_DISCORD_BOT_TOKEN")
+	if not bot_token:
+		raise ValueError("No discord bot token provided.")
+	client.run(bot_token)
 
 if __name__ == "__main__":
 	main()
