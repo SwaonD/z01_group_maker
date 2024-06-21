ac2a840c61c07cdd0ec7c876a109865aed1174d8
Farès Aoudia <fares.aoudia0@gmail.com>
2024-06-21 15:50:26 +0200

    Create group function

--- src/group_core.py
+++ src/group_core.py
@@ -1,5 +1,7 @@
 import discord
 
 async def ping(interaction: discord.Interaction, note: str):
-	print("coucou")
 	await interaction.response.send_message(f'pong: {note}')
+
+async def createGroup(interaction: discord.Interaction, project_name: str):
+    await interaction.response.send_message(f'{interaction.user.mention} wants to create a group for {project_name}')
