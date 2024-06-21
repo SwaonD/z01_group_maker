1df27f4b581e032b9cee4ff2e062253bda0b2b52
Farès Aoudia <fares.aoudia0@gmail.com>
2024-06-21 15:50:26 +0200

    Create group function

--- src/commands.py
+++ src/commands.py
@@ -1,13 +1,17 @@
 import discord
 from typing import Optional, Union
 from discord.app_commands import CommandTree
-from src.group_core import ping
+from src.group_core import ping, createGroup
 from src.settings.variables import GUILD_ID
 
 def register_commands(tree: CommandTree):
 	@tree.command(name="ping", description="ping", guild=discord.Object(id=GUILD_ID))
 	async def ping_command(interaction: discord.Interaction, note: Optional[str]):
 		await ping(interaction, note)
+  
+	@tree.command(name="create", description="Create a group", guild=discord.Object(id=GUILD_ID))
+	async def create_command(interaction: discord.Interaction, project_name: str):
+		await createGroup(interaction, project_name)
 
 # NOTES:
 # @tree.command(name="delete_message", description="delete_message", guild=discord.Object(id=GUILD_ID))
