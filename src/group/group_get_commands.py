30b324c3bfac5c905703bd18e3e49fa92bef32b6
Swaon <swaon@hotmail.com>
2024-06-22 23:32:06 +0200

    channel var to .env

--- src/group/group_get_commands.py
+++ src/group/group_get_commands.py
@@ -10,16 +10,16 @@ async def list(ctx: Interaction, project_name: str | None):
 	data = GROUPS_TABLE.get_data(
 			condition, GROUPS_TABLE.project_name,
 			GROUPS_TABLE.creator_id, GROUPS_TABLE.message_id)
-	embed = Embed(color=Colour.from_rgb(255, 0, 0), )
+	embed = Embed(color=Colour.from_rgb(255, 0, 0))
 	content = []
 	for tup in data:
 		user = ctx.client.get_user(tup[1])
 		if user is None:
 			user = await ctx.client.fetch_user(tup[1])
-		group_channel: TextChannel = ctx.client.get_channel(GROUP_CHANNEL_ID_L)
+		group_channel: TextChannel = ctx.client.get_channel(GROUP_CHANNEL_ID)
 		if group_channel is None:
 			group_channel: TextChannel = \
-					await ctx.client.fetch_channel(GROUP_CHANNEL_ID_L)
+					await ctx.client.fetch_channel(GROUP_CHANNEL_ID)
 		message: PartialMessage = group_channel.get_partial_message(tup[2])
 		content.append([tup[0], user.mention, message.jump_url])
 	if len(content) == 0:
--- src/settings/variables.py
+++ src/settings/variables.py
@@ -1,6 +1,9 @@
+import os
+
 GUILD_ID = 352509375266422797
-GROUP_CHANNEL_ID_S = 1253719751541264434
-GROUP_CHANNEL_ID_L = 1253698115425533953
+GROUP_CHANNEL_ID = os.getenv("Z_01_GROUP_CHANNEL_ID")
+# commands1 1253719751541264434
+# commands 1253698115425533953
 GROUP_SQL_FILE = "./src/settings/group_data.db"
 LOG_PATH="/home/liamine/projects/z01_group_maker/log/logfile.log"
 
