a72393aa1d2e31d87427b79d3d1b3679d493af18
Swaon <swaon@hotmail.com>
2024-06-22 23:32:06 +0200

    channel var to .env

--- src/group/group_core.py
+++ src/group/group_core.py
@@ -18,32 +18,32 @@ class MyView(ui.View):
 	@ui.button(label="Join", style=ButtonStyle.primary)
 	async def callback1(self, ctx: Interaction, button: Button):
 		id = get_group_id(ctx.message.id)
-  
+
 		if is_member(id, ctx.user.id):
 			await ctx.response.send_message(":x: You are already in this group !", ephemeral=True)
 			return
-  
+
 		GROUP_MEMBERS_TABLE.insert_data(id, ctx.user.id)
-  
+
 		await update_members_count(ctx, self.project, self.author)
 		await ctx.response.send_message(f"{ctx.user.mention} joined {self.author.mention}'s group for {self.project}", ephemeral=True)
 
 	@ui.button(label="Leave", style=ButtonStyle.secondary)
 	async def callback2(self, ctx: Interaction, button: Button):
 		id = get_group_id(ctx.message.id)
-  
+
 		if is_member(id, ctx.user.id) is False:
 			await ctx.response.send_message(":x: You are not in this group !", ephemeral=True)
 			return
 
 		GROUP_MEMBERS_TABLE.delete_data(f"{GROUP_MEMBERS_TABLE.id} = {id} AND {GROUP_MEMBERS_TABLE.user_id} = {ctx.user.id}")
-		
+
 		await update_members_count(ctx, self.project, self.author)
 		await ctx.response.send_message(f"{ctx.user.mention} left {self.author.mention}'s group for {self.project}", ephemeral=True)
 
 async def update_members_count(ctx: Interaction, project: str, author: Union[User, Member]):
 	group_members = get_group_members(ctx.message.id)
- 
+
 	if len(group_members) == 0:
 		GROUPS_TABLE.delete_data(f"{GROUPS_TABLE.id} = {get_group_id(ctx.message.id)}")
 		channel: TextChannel = await ctx.client.fetch_channel(GROUP_CHANNEL_ID)
@@ -54,19 +54,19 @@ async def update_members_count(ctx: Interaction, project: str, author: Union[Use
 	embed_desc: str = f'''
 	{author.mention} created a group for ```{project}```
 	'''
-	embed: Embed = Embed( 
+	embed: Embed = Embed(
 		description=embed_desc,
 		title="Group Creation",
 		colour=Colour.from_str("#FFF"),
 		type="rich"
 	)
- 
+
 	usernames: str = ""
 	for m in group_members:
 		usernames += ctx.client.get_user(m[0]).mention + "\n"
-     
+
 	embed.add_field(name="Members", value=usernames, inline=False)
-  
+
 	try:
 		channel: TextChannel = await ctx.client.fetch_channel(GROUP_CHANNEL_ID)
 		message = await channel.fetch_message(ctx.message.id)
@@ -102,7 +102,7 @@ async def create_group(ctx: Interaction, project: str):
 
 	embed.add_field(name="Members", value=ctx.user.mention, inline=False)
 	log(f'{ctx.user} created a group for {project}', False)
- 
+
 	v = MyView(project, ctx.user)
 
 	# Send the embed and get the message object
