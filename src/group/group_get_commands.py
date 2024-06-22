83ae3cecc653e3cda841c7d9f66b7dea6f0d556a
Swaon <swaon@hotmail.com>
2024-06-22 18:10:47 +0200

    better list

--- src/group/group_get_commands.py
+++ src/group/group_get_commands.py
@@ -1,6 +1,7 @@
 from discord import Interaction, Embed, Colour, TextChannel, PartialMessage
 from src.settings.tables import GROUPS_TABLE
 from src.settings.variables import GROUP_CHANNEL_ID
+from src.utils.str import get_aligned_text
 
 async def list(ctx: Interaction, project_name: str | None):
 	condition = ""
@@ -9,8 +10,8 @@ async def list(ctx: Interaction, project_name: str | None):
 	data = GROUPS_TABLE.get_data(
 			condition, GROUPS_TABLE.project_name,
 			GROUPS_TABLE.creator_id, GROUPS_TABLE.message_id)
-	embed = Embed(color=Colour.from_rgb(255, 0, 0))
-	content = ""
+	embed = Embed(color=Colour.from_rgb(255, 0, 0), )
+	content = []
 	for tup in data:
 		user = ctx.client.get_user(tup[1])
 		if user is None:
@@ -20,10 +21,10 @@ async def list(ctx: Interaction, project_name: str | None):
 			group_channel: TextChannel = \
 					await ctx.client.fetch_channel(GROUP_CHANNEL_ID)
 		message: PartialMessage = group_channel.get_partial_message(tup[2])
-		content += f"{tup[0]} {user.mention} {message.jump_url}\n"
-	if content == "":
+		content.append([tup[0], user.mention, message.jump_url])
+	if len(content) == 0:
 		await ctx.response.send_message(f"Project {project_name} not found.")
 	else:
-		embed.description = content
+		embed.description = get_aligned_text(content, " ", 1) # "\u2002"
 		await ctx.response.send_message(embed=embed, ephemeral=True)
 
