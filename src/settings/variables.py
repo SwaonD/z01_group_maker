cdbc79239dc53b591d522bbe1f380b838310a8fe
Swaon <swaon@hotmail.com>
2024-06-22 23:39:27 +0200

    db into .env

--- src/settings/variables.py
+++ src/settings/variables.py
@@ -4,8 +4,8 @@ GUILD_ID = 352509375266422797
 GROUP_CHANNEL_ID = os.getenv("Z_01_GROUP_CHANNEL_ID")
 # commands1 1253719751541264434
 # commands 1253698115425533953
-GROUP_SQL_FILE = "./src/settings/group_data.db"
-LOG_PATH="/home/liamine/projects/z01_group_maker/log/logfile.log"
+GROUP_SQL_FILE = os.getenv("Z_01_GROUP_DB")
+LOG_PATH="./log/logfile.log"
 
 PROJECT_NAMES = [
 	"ascii-art",
