1ae5b841d5d75d3382a7fd2ce0b06d6ea23f248f
Swaon <swaon@hotmail.com>
2024-06-23 14:55:23 +0200

    fix rebase

--- src/group/message/tools.py
+++ src/group/message/tools.py
@@ -1,5 +1,5 @@
 from src.settings.tables import GROUP_MEMBERS_TABLE, GROUPS_TABLE
-from src.settings.tables import GROUP_MEMBERS_TABLE, GROUPS_TABLE
+from src.settings.variables import PROJECT_NAMES
 from dataclasses import dataclass
 
 
@@ -61,4 +61,34 @@ def is_member(group_id: int, user_id: int):
 	for m in members:
 		if m[0] == user_id:
 			return True
+	return False
+
+def is_project(project: str) -> bool:
+	"""Verifies if a project is valid
+
+	Args:
+		project (str): project name to be checked
+
+	Returns:
+		bool: returns True if the project is a valid one
+	"""
+	for p in PROJECT_NAMES:
+		if p == project:
+			return True
+	return False
+
+def project_exists(project: str, author_id: int) -> bool:
+	"""Verifies in the database if a project was already created by the user
+
+	Args:
+		project (str): project name
+		author (str): projects's author
+
+	Returns:
+		bool: returns True is the project was already found
+	"""
+	group_id = GROUPS_TABLE.get_data(f"{GROUPS_TABLE.project_name} = '{project}' AND {GROUPS_TABLE.creator_id} = '{author_id}'", GROUPS_TABLE.id)
+	print(f"[*] Checked if project {project} already exists : {len(group_id) != 0}")
+	if len(group_id) != 0:
+		return True
 	return False
