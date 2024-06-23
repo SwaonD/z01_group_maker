d87e894bfa0366f0bfc293aeebfaeb75f0f62d41
Swaon <swaon@hotmail.com>
2024-06-23 15:50:04 +0200

    start status

--- src/commands.py
+++ src/commands.py
@@ -2,13 +2,14 @@ from discord import Interaction, Object
 from typing import Optional
 from discord.app_commands import CommandTree
 from src.group.commands.create_group import create_group
+from src.group.commands.status import status
 from src.group.commands.list import list
 from src.settings.variables import GUILD_ID
 
 def register_commands(tree: CommandTree):
-	@tree.command(name="ping", description="ping", guild=Object(id=GUILD_ID))
-	async def ping_command(ctx:Interaction):
-		await ping(ctx)
+	# @tree.command(name="ping", description="ping", guild=Object(id=GUILD_ID))
+	# async def ping_command(ctx:Interaction):
+	# 	await ping(ctx)
 
 	@tree.command(name="create",
 			description="Create a group", guild=Object(id=GUILD_ID))
@@ -20,11 +21,10 @@ def register_commands(tree: CommandTree):
 	async def list_command(ctx: Interaction, name: Optional[str]):
 		await list(ctx, name)
 
-async def ping(ctx:Interaction):
-	await ctx.response.send_message("pong")
+	@tree.command(name="status",
+			description="Display your infos", guild=Object(id=GUILD_ID))
+	async def status_command(ctx: Interaction):
+		await status(ctx)
 
-# NOTES:
-# @tree.command(name="delete_message", description="delete_message", guild=discord.Object(id=GUILD_ID))
-# async def delete_message_command(interaction: discord.Interaction, user: Optional[str], all_user: Optional[bool],
-# 				channel: Optional[Union[discord.TextChannel, discord.VoiceChannel]]):
-# 	await free_server.message_managment(interaction, user, all_user, channel, to_delete=True)
+# async def ping(ctx:Interaction):
+# 	await ctx.response.send_message("pong")
