3124a10c7f862d06b79923a83dddd75cc66fa37b
Swaon <swaon@hotmail.com>
2024-06-28 17:12:32 +0200

    add update channel id on config exists

--- src/group/commands/config.py
+++ src/group/commands/config.py
@@ -23,6 +23,11 @@ from src.utils.discord import send_quick_response
 # 	await send_quick_response(ctx, select.values[0])
 
 async def config(ctx: Interaction, group_channel: TextChannel):
-	GROUPS_CONFIG.insert_data(ctx.guild_id, group_channel.id)
+	data = GROUPS_CONFIG.get_data(f"{GROUPS_CONFIG.guild_id} = {ctx.guild.id}")
+	if len(data) == 0:
+		GROUPS_CONFIG.insert_data(ctx.guild_id, group_channel.id)
+	else:
+		GROUPS_CONFIG.update_data({GROUPS_CONFIG.group_channel_id: \
+				group_channel.id}, f"{GROUPS_CONFIG.guild_id} = {ctx.guild.id}")
 	await send_quick_response(ctx, \
-			MSG.GROUP_CHANNEL_CONFIGURED % (group_channel.mention))
+				MSG.GROUP_CHANNEL_CONFIGURED % (group_channel.mention))
