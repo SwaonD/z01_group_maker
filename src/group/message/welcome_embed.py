9624db425ac2eb497b6846b52425fd17962d1ada
Swaon <swaon@hotmail.com>
2024-07-03 00:22:05 +0200

    remove reload_group command in wlcome message

--- src/group/message/welcome_embed.py
+++ src/group/message/welcome_embed.py
@@ -7,7 +7,7 @@ class WelcomeMessageEmbed(Embed):
 		self.description = """
 		This bot helps creating groups to work together !
 		:bangbang: **Before using this bot don't forget to set up a channel ID with the /config <channel_id> command !**
-  		:page_facing_up: Here is a list of the commands available: 
+  		:page_facing_up: Here is a list of the commands available:
   		"""
 		self.title = f"Welcome Group Maker !"
 		self.type = "rich"
@@ -16,4 +16,3 @@ class WelcomeMessageEmbed(Embed):
 		self.add_field(name="/status", value="Displays the current groups you are in or the groups you created", inline=False)
 		self.add_field(name="/config", value="Takes as argument the channel ID you want the bot to send messages on", inline=False)
 		self.add_field(name="/kick", value="Kicks someone from a group whose leader is you, takes as argument a project name and a user to kick", inline=False)
-		self.add_field(name="/reload_groups", value="Reload all the group messages if the buttons dont work", inline=False)
