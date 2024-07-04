4330b4c85c9ace964052296f99118b50183a5ccf
Farès Aoudia <fares.aoudia0@gmail.com>
2024-07-04 14:05:03 +0200

    Private message function to avoid 403

--- src/utils/discord.py
+++ src/utils/discord.py
@@ -1,7 +1,16 @@
-from discord import Interaction
+from discord import Interaction, Member, errors
 from src.settings.variables import NOTIF_MSG_TIMEOUT
+from src.utils.log import LOGGER
 
 async def send_quick_response(ctx: Interaction, \
 		msg: str, timeout: int = NOTIF_MSG_TIMEOUT):
 	await ctx.response.send_message(msg,
 			ephemeral=True, delete_after=timeout)
+ 
+async def send_private_message(member: Member, \
+		msg: str, timeout: int = NOTIF_MSG_TIMEOUT):
+	try:
+		await member.send(msg, suppress_embeds=True)
+	except errors.Forbidden:
+		LOGGER.msg(f"Could not send message to" \
+			+ f"{member.name} on {member.guild.name}")
